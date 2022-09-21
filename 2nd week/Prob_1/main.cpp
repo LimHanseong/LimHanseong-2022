e065e5e6b54fedd99a12ec678e2432057e4eafdc
LimHanseong <hanseong2049@gmail.com>
2022-09-21 10:12:29 +0900

    Prob_1 int
    printf practice

---
+++ 2nd week/Prob_1/main.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Student number : %d\n", 2019011519); // Output student neumber
+	printf("Name : %s\n", "LimHansoeng");        // Output name
+	printf("Credit : %c\n", 'A');                // Output credit
+	return 0;
+}
