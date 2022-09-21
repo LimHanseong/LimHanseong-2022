60a5cfc8f95739c9a0c397d2b00f49454fa673a6
LimHanseong <hanseong2049@gmail.com>
2022-09-21 11:36:49 +0900

    int
    - 1nd week prob int
    - 2nd week prob_2 debug completion

--- 2nd week/Prob_2/main.cpp
+++ 2nd week/Prob_2/main.cpp
@@ -5,7 +5,7 @@ int main()
 	char ch;                      // Specify a variable to store characters with ch
 
 	printf("Character input : "); // Output input information message
-	scanf_s("%c", &ch);           // Scan typed characters
+	scanf_s("%c", &ch, 2);           // Scan typed characters
 	printf("The ASCII code value of the '%c' character is '%d'.", ch, ch); // Convert and inform
 	return 0;
 }
